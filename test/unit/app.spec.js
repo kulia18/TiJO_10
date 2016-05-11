410f031a38816a4fad77636be0ce81b69d98d0e0
Michal Kulig <paweloleksy112@gmail.com>
2016-05-11 12:15:12 +0200

    Test

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -19,6 +19,35 @@ describe('app', function () {
                 name: 'Zack', age: 5, course: 'kids'
             });
         });
+        it('should assign person below 12 to kids group', function () {
+            expect(app.assignToSwimmingCourse('Zack', '10/06/2010')).toEqual({
+                name: 'Zack', age: 5, course: 'kids'
+            });
+        });
+    });
+
+    describe('calculateArea', function () {
+        it('should return false, if the third parametr is not a number', function () {
+            expect(app.calculateArea(1, 15, 'x', 'Zle', 'Dobrze')).toBe(false);
+        });
+        it('should return false, if the fifth parametr is not a string', function () {
+            expect(app.calculateArea(5, 5, 5, 'Dobrze', 999)).toBe(false);
+        });
+        it('should return false, if there is not enough parametrs', function () {
+            expect(app.calculateArea(1, 1)).toBe(false);
+        });
+        it('should return false, if the first parametr is not a number', function () {
+            expect(app.calculateArea('x', 'x', 5, 'Dobrze', 'Zle')).toBe(false);
+        });
+        it('should return Dobrze message, if all parametrss are proper', function () {
+            expect(app.calculateArea(30, 10, 1, 'Dobrze', 'Zle')).toEqual({ area : 20, message : 'Dobrze' });
+        });
+        it('should return Zle message, if all paramsetrs are proper', function () {
+            expect(app.calculateArea(10, 10, 2, 'Dobrze', 'Zle')).toEqual({ area : -10, message : 'Zle' });
+        });
+        it('should return Big null message, if all parametrss are proper', function () {
+            expect(answer.calculateArea(0, 0, 0, 'Dobrze', 'Zle')).toEqual({area: 0, message: 'Big null'});
+        });
     });
 });
 
